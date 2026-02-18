ea0bf1cb8c98ddb1b4ef69bf8be0f361d43deb16
meghlesh <meghachaudhari24@gmail.com>
2026-02-18 13:09:43 +0530

    Update index.js

--- CWS-Server/index.js
+++ CWS-Server/index.js
@@ -9939,7 +9939,7 @@ app.post("/performance/:id/reject", authenticate, async (req, res) => {
 
 connectDB();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
